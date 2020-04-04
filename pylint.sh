f6a93c290805d2a268315cbd7a20ff6b462f07e9
gousaiyang <gousaiyang@163.com>
2020-04-04 15:23:04 -0700

    Enhance CI

---
+++ pylint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+shopt -s dotglob globstar nullglob
+pylint --load-plugins=pylint.extensions.check_elif,pylint.extensions.docstyle,pylint.extensions.emptystring,pylint.extensions.overlapping_exceptions \
+       --disable=all \
+       --enable=F,E,W,R,basic,classes,format,imports,refactoring,else_if_used,docstyle,compare-to-empty-string,overlapping-except \
+       --disable=blacklisted-name,invalid-name,missing-class-docstring,missing-function-docstring,missing-module-docstring,design,too-many-lines,old-division,no-absolute-import,input-builtin,too-many-nested-blocks \
+       --max-line-length=120 \
+       --init-import=yes \
+       ./**/*.py ./**/*.pyw ./**/*.py3 ./**/*.pyi
