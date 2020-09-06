b373a2ba9828e209750772abb9f3c7c1c13b16f3
gousaiyang <gousaiyang@163.com>
2020-09-06 11:28:43 -0700

    Fix test_typing.py

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -2,4 +2,4 @@ from bpc_utils import Linesep
 
 
 def test_Linesep() -> None:
-    assert Linesep.__class__.__module__ == 'typing'  # nosec
+    assert Linesep.__class__.__module__ in {'typing', 'typing_extensions'}  # nosec
