ef6eefbf7bad94cc09b452d0c0c77451a3794642
gousaiyang <gousaiyang@163.com>
2020-09-07 16:58:07 -0700

    Fix Flake8

--- bpc_utils/misc.py
+++ bpc_utils/misc.py
@@ -17,7 +17,7 @@ def first_truthy(*args: T) -> Optional[T]:
     ...
 
 
-@overload
+@overload  # noqa: F811
 def first_truthy(args: Iterable[T]) -> Optional[T]:  # noqa: F811
     ...
 
@@ -50,7 +50,7 @@ def first_non_none(*args: T) -> Optional[T]:
     ...
 
 
-@overload
+@overload  # noqa: F811
 def first_non_none(args: Iterable[T]) -> Optional[T]:  # noqa: F811
     ...
 
