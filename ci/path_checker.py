379fd3b0589b45c0f771196632e9aa629ccd6e2b
gousaiyang <gousaiyang@163.com>
2020-10-14 15:27:57 -0700

    Avoid short-circuiting in linter execution

--- ci/path_checker.py
+++ ci/path_checker.py
@@ -6,8 +6,7 @@ import sys
 module_name = 'bpc_utils'
 
 if not os.path.isfile('setup.py'):
-    print('Please execute this script in the project root directory.', file=sys.stderr)
-    sys.exit(1)
+    sys.exit('Please execute this script in the project root directory.')
 
 sys.path.insert(0, '.')
 
@@ -15,5 +14,4 @@ path = importlib.import_module(module_name).__file__
 print('Module {} is located at: {}'.format(module_name, path))
 
 if len(sys.argv) > 1 and sys.argv[1] == 'strict' and not re.search(r'\b(?:site|dist)-packages\b', path):
-    print('Error: module {} is not an installed version'.format(module_name), file=sys.stderr)
-    sys.exit(1)
+    sys.exit('Error: module {} is not an installed version'.format(module_name))
