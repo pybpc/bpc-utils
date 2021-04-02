e955ab295deb14dd1e58de22826ee6a3c50eca2c
gousaiyang <gousaiyang@163.com>
2021-04-01 19:22:50 -0700

    Version 0.10.1

--- bpc_utils/__init__.py
+++ bpc_utils/__init__.py
@@ -11,7 +11,7 @@ from .multiprocessing import *
 from .parsing import *
 from .typing import *
 
-__version__ = '0.10.0'
+__version__ = '0.10.1'
 
 __all__ = ['__version__', 'parse_positive_integer', 'parse_boolean_state', 'parse_linesep', 'parse_indentation',
            'BaseContext', 'detect_files', 'archive_files', 'recover_files', 'BPCRecoveryError', 'first_truthy',
